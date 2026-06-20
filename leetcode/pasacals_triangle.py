e39589aff703a55e34c08055a05f4675d95f688e
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-06-20 20:19:19 +0530

    Pasacals Triangle

---
+++ leetcode/pasacals_triangle.py
@@ -0,0 +1,16 @@
+'''PROBLEM - Pascal's Triangle
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+'''
+
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        triangle = []
+        
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
+            
+        return triangle
