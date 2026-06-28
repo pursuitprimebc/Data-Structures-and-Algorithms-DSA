832961ba06c5d6dc80286ea0bb0c01e21785b90f
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-06-28 22:01:09 +0530

    journey

--- codeforces/journey.py
+++ codeforces/journey.py
@@ -1,21 +1,21 @@
-n,a,b,c = map(int, input().split())
-days = 0
-count = 0
-dist_in_3days = a + b + c
-        
-full_cycles = n // dist_in_3days
-rem_dist = n % dist_in_3days
-
-if rem_dist == 0:
-    days = full_cycles * 3
-else:
-    days = full_cycles * 3
+t = int(input())
+for i in range(t):
+    n,a,b,c = map(int, input().split())
+    days = 0
+    count = 0
+    dist_in_3days = a + b + c
             
-if rem_dist <= a:
-    days += 1
-elif rem_dist <= a + b:
-    days += 2
-else:
-    days += 3
+    full_cycles = n // dist_in_3days
+    rem_dist = n % dist_in_3days
 
-print(days)
+    days = full_cycles * 3
+
+    if rem_dist > 0:
+        if rem_dist <= a:
+            days += 1
+        elif rem_dist <= a + b:
+            days += 2
+        else:
+            days += 3
+
+    print(days)
