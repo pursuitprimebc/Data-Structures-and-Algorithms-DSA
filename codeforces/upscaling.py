6a1bba2ffbf49218f996dae57b3fbf23ecceb70f
pursuitprimebc <bibhashchandra4850@gmail.com>
2026-08-23 22:16:55 +0530

    1950B Upscaling

--- codeforces/upscaling.py
+++ codeforces/upscaling.py
@@ -3,11 +3,11 @@ t = int(input())
 for i in range(t):
     n = int(input())
 
-    row1 = ("##.." * n)[:2 * n]
-    row2 = ("..##" * n)[:2 * n]
+    r1 = ("##.." * n)[:2 * n]
+    r2 = ("..##" * n)[:2 * n]
 
     for i in range(2 * n):
         if (i // 2) % 2 == 0:
-            print(row1)
+            print(r1)
         else:
-            print(row2)
+            print(r2)
